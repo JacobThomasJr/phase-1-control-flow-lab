95387d1ba6f3d7b9962ab023296b024aece0ee61
jacobaranza <jacobthomasjunior@gmail.com>
2023-04-02 15:04:23 -0500

    Control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet) {
+  let result;
+  
+  if (distanceInFeet <= 400) {
+    result = 'This one is on me!';
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    result = 'That will be twenty bucks.';
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  } else {
+    result = 'No can do.';
+  }
+
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+  
 }
